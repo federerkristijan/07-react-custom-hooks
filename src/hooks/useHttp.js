8c252029ec29b1db5d885759a4b3deffeb1b1371
Kristijan Federer <federer.kristijan@gmail.com>
2022-07-21 13:42:00 +0200

    fetch big fixed, changed url in newtasks.js

--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -1,21 +1,22 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = async (taskText) => {
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch(
         requestConfig.url, {
-          method: requestConfig.method,
-          headers: requestConfig.headers,
-          body: JSON.stringify(requestConfig.body)
-        }
-        // 'https://udemy-react-21466-default-rtdb.europe-west1.firebasedatabase.app/tasks.json'
-      );
+          //  if the method is not set, send GET
+          method: requestConfig.method ? requestConfig.method : 'GET',
+          // if the headers are not set, set it to an empty object
+          headers: requestConfig.headers ? requestConfig.headers : {},
+          // if the body is set, stringify it, if not set it to null
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+        });
 
       if (!response.ok) {
         throw new Error('Request failed!');
@@ -23,13 +24,11 @@ const useHttp = (requestConfig, applyData) => {
 
       const data = await response.json();
       applyData(data);
-
-
     } catch (err) {
       setError(err.message || 'Something went wrong!');
     }
     setIsLoading(false);
-  };
+  }, []);
 
   return {
     // v.1
